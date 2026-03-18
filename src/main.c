2a1c7e0bfbbca40c08a1070ec8e21a2c96de1825
Konrad Guzek <konrad@guzek.uk>
2026-03-18 14:32:42 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void collect_input(char **input, size_t *size) {
   *size = 0;
@@ -23,6 +24,29 @@ const char *BUILTIN_COMMANDS[] = {"exit", "echo", "type"};
 const int BUILTIN_COMMANDS_LENGTH =
     sizeof(BUILTIN_COMMANDS) / sizeof(BUILTIN_COMMANDS[0]);
 
+char *get_full_path(const char *command) {
+  char *path = getenv("PATH");
+  if (!path) {
+    return NULL;
+  }
+
+  char *path_copy = strdup(path);
+  char *saveptr = NULL;
+  char *path_dir = strtok_r(path_copy, ":", &saveptr);
+  while (path_dir) {
+    path_dir = strtok_r(NULL, ":", &saveptr);
+    char full_path[1024];
+    snprintf(full_path, sizeof(full_path), "%s/%s", path_dir, command);
+    if (access(full_path, X_OK) == 0) {  // checks if executable
+      free(path_copy);
+      return strdup(full_path);
+    }
+  }
+  free(path_copy);
+
+  return NULL;
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -40,18 +64,24 @@ int main(int argc, char *argv[]) {
       }
     } else if (strncmp(input, "type", 4) == 0) {
       if (strlen(input) > 5) {
+        const char *command_arg = input + 5;
         const char *command_match = NULL;
         for (int i = 0; i < BUILTIN_COMMANDS_LENGTH; i++) {
           const char *command = BUILTIN_COMMANDS[i];
-          if (strcmp(command, input + 5) == 0) {
+          if (strcmp(command, command_arg) == 0) {
             command_match = command;
             break;
           }
         }
-        if (command_match == NULL) {
-          printf("%s: not found", input + 5);
-        } else {
+        if (command_match != NULL) {
           printf("%s is a shell builtin", command_match);
+        } else {
+          char *full_path = get_full_path(command_arg);
+          if (full_path == NULL) {
+            printf("%s: not found", command_arg);
+          } else {
+            printf("%s is %s", command_arg, full_path);
+          }
         }
       } else {
         printf(": not found");
